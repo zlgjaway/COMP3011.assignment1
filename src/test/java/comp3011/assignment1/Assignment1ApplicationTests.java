9133eb95ef5cf03672de1fcc2377cdb5b0169283
zlgjaway <long252005@gmail.com>
2026-09-13 19:55:00 +0930

    Initial Spring Boot project

---
+++ src/test/java/comp3011/assignment1/Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package comp3011.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
